e792659fa0643abf8b4d82a81ff893e636f8b833
Mauricio Argumedo <mauargudev@gmail.com>
2024-08-30 11:57:52 -0600

    Delete destination implemented

--- iTour/ContentView.swift
+++ iTour/ContentView.swift
@@ -25,6 +25,7 @@ struct ContentView: View {
                         Text(destination.date.formatted(date: .long, time: .shortened))
                     }
                 }
+                .onDelete(perform: deleteDestinations)
             }
             .navigationTitle("iTour")
             .toolbar {
@@ -40,6 +41,13 @@ struct ContentView: View {
         modelContext.insert(rome)
         modelContext.insert(florence)
         modelContext.insert(naples)
+    }
+    
+    func deleteDestinations(_ indexSet: IndexSet) {
+        for index in indexSet {
+            let destination = destinations[index]
+            modelContext.delete(destination)
+        }
     }
 }
 
