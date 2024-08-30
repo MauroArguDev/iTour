8a4ced0ddf311e26cba5e4d5028e1d64f8b3db86
Mauricio Argumedo <mauargudev@gmail.com>
2024-08-30 11:50:27 -0600

    Save mock destinations and query them

--- iTour/ContentView.swift
+++ iTour/ContentView.swift
@@ -5,20 +5,45 @@
 //  Created by Mauricio Argumedo on 30/8/24.
 //
 
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - Properties
+    @Environment(\.modelContext) var modelContext
+    @Query var destinations: [Destination]
+    
+    // MARK: - Body
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(destinations) { destination in
+                    VStack(alignment: .leading) {
+                        Text(destination.name)
+                            .font(.headline)
+                        
+                        Text(destination.date.formatted(date: .long, time: .shortened))
+                    }
+                }
+            }
+            .navigationTitle("iTour")
+            .toolbar {
+                Button("Add Samples", action: addSamples)
+            }
         }
-        .padding()
+    }
+    
+    func addSamples() {
+        let rome = Destination(name: "Rome")
+        let florence = Destination(name: "Florence")
+        let naples = Destination(name: "Naples")
+        modelContext.insert(rome)
+        modelContext.insert(florence)
+        modelContext.insert(naples)
     }
 }
 
+// MARK: - Preview
 #Preview {
     ContentView()
 }
