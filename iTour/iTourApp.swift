6424166a1e2c39f8e7db2ae72974d5ba8d5000aa
Mauricio Argumedo <mauargudev@gmail.com>
2024-08-30 11:37:03 -0600

    Data model definition

--- iTour/iTourApp.swift
+++ iTour/iTourApp.swift
@@ -5,6 +5,7 @@
 //  Created by Mauricio Argumedo on 30/8/24.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
@@ -13,5 +14,6 @@ struct iTourApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: Destination.self)
     }
 }
